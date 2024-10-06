3b22e1cfdcae70e6117a0cd490af8af96ea394f3
Vivi Poit <vwpoit@gmail.com>
2024-10-06 18:42:49 -0400

    add role to user table and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
-  normalizes :email_address, with: -> e { e.strip.downcase }
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  enum :role, { administration: 1, faculty: 2, student: 3 }
 end
