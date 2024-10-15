64458ab362114083743355b65e3033a99f35f269
Vivi Poit <vwpoit@gmail.com>
2024-10-15 15:17:01 -0400

    create TelegramClient

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'webmock/rspec'
+
+require 'dotenv'
+Dotenv.load('.env.example')
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
